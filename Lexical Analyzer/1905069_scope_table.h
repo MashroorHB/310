48bce4fe4f2b9e95917cdb8e72a9424001aab169
Mashroor Hasan Bhuiyan <mashroor184@gmail.com>
2023-01-24 20:28:05 +0600

    lex

---
+++ Lexical Analyzer/1905069_scope_table.h
@@ -0,0 +1,141 @@
+#pragma once
+#include<bits/stdc++.h>
+#include "1905069_symbol_info.h"
+using namespace std;
+typedef long long int ll;
+typedef double db;
+
+
+
+class ScopeTable{
+private:
+    ll bucketSize;
+    ll symbolCount;
+    ScopeTable* parentScope;
+    SymbolInfo** bucket;
+    ll id;
+public:
+    ScopeTable(ll bucketSize, ScopeTable* parent){
+        this->bucketSize = bucketSize;
+        symbolCount = 0;
+        parentScope = parent;
+        bucket = new SymbolInfo*[bucketSize];
+        for(ll i=0; i<bucketSize; i++) bucket[i]=nullptr;
+        id = cnt+1;
+        cnt++;
+    }
+    ll sdbm_hash(string s) {
+        ll h = 0, len = s.size();
+        for (ll i=0; i<len; i++){
+            h =((s[i])+(h<<6)+(h<<16)-h);
+        }
+        //cout<< s<< " "<<  h<< endl;
+        return ((h%bucketSize)+bucketSize)%bucketSize;
+    }
+    bool insert(string name, string type){
+
+        SymbolInfo* newSymbol = this->lookUp(name,false);
+        if(newSymbol) return false;
+
+        newSymbol = new SymbolInfo(name,type);
+        ll h = sdbm_hash(name);
+        ll pos=1;
+        if(!bucket[h]) bucket[h] = newSymbol;
+        else{
+            SymbolInfo* curr = bucket[h];
+            pos++;
+            while(curr->getNext()){
+                curr=curr->getNext();
+                pos++;
+            }
+            curr->setNext(newSymbol);
+        }
+        symbolCount++;
+        return true;
+    }
+
+    SymbolInfo* lookUp(string name, bool show = true){
+        ll h = sdbm_hash(name);
+        ll pos=1;
+        SymbolInfo* curr = bucket[h];
+
+
+        if(!curr){
+            //if(show) cout<< "\t'"<< name<< "' not found in any of the ScopeTables\n";
+            return nullptr;
+        }
+
+        while(curr && curr->getName()!=name){
+            pos++;
+            curr = curr->getNext();
+        }
+        if(!curr){
+            //if(show) cout<< "\t'"<< name<< "' not found in any of the ScopeTables\n";
+            return nullptr;
+        }
+        //if(show) cout<< "\t'"<< name<< "' found in ScopeTable# "<< this->id<< " at position "<< h+1<< ", "<< pos<< "\n";
+        return curr;
+    }
+
+    bool remove(string name){
+        ll h = sdbm_hash(name);
+        ll pos = 1;
+        SymbolInfo* curr = bucket[h];
+
+        if(!curr)  return false;
+
+        if(bucket[h]->getName()==name){
+            (bucket[h]->getNext())? bucket[h]=bucket[h]->getNext() : bucket[h]=nullptr;
+            return true;
+        }
+        pos++;
+        while(curr->getNext() && curr->getNext()->getName()!=name){
+            curr = curr->getNext();
+            pos++;
+        }
+        if(!curr->getNext()) return false;
+        SymbolInfo* dlt = curr->getNext();
+        curr->setNext(curr->getNext()->getNext());
+        return true;
+    }
+
+    void print(ostream& os){
+        os<< "\tScopeTable# "<< id<< "\n";
+        for(ll i=0; i<bucketSize; i++){
+            SymbolInfo* curr = bucket[i];
+            if(!curr) continue;
+            os<< "\t"<< i+1<< "--> ";
+            while(curr){
+                os<< *curr;
+                curr = curr->getNext();
+            }
+            os<< "\n";
+        }
+    }
+
+    friend ostream& operator<<(ostream& os, const ScopeTable dt){
+        for(ll i=0; i<dt.bucketSize; i++){
+            SymbolInfo* curr = dt.bucket[i];
+            if(!curr) continue;
+            while(curr){
+                cout<< " "<< *curr;
+                curr = curr->getNext();
+            }
+            cout<< endl;
+        }
+        return os;
+    }
+    ScopeTable* getParent(){
+        return parentScope;
+    }
+    ll getID(){
+        return id;
+    }
+
+    ~ScopeTable(){
+        delete [] bucket;
+    }
+    static ll cnt;
+};
+
+ll ScopeTable::cnt = 0;
---
+++ Lexical Analyzer/1905069_symbol_table.h
@@ -0,0 +1,92 @@
+#pragma once
+#include<bits/stdc++.h>
+#include "1905069_scope_table.h"
+using namespace std;
+typedef long long int ll;
+typedef double db;
+
+
+class SymbolTable{
+private:
+    ll tableSize;
+    ScopeTable* top;
+
+public:
+    SymbolTable(ll n){
+        this->tableSize = n;
+        top = new ScopeTable(tableSize,nullptr);
+        ScopeTable::cnt = 1;
+    }
+    void enterScope(){
+        ScopeTable* curr = new ScopeTable(tableSize, top);
+        top = curr;
+    }
+    void exitScope(){
+        if(top->getParent()){
+            ScopeTable* curr = top->getParent();
+            delete top;
+            top = curr;
+        }
+        else{
+            cout<< "\tScopeTable# "<< top->getID()<< " cannot be removed\n";
+        }
+    }
+    bool insert(string name, string type, ostream& os = cout){
+        if(!top->lookUp(name, false)){
+            return top->insert(name,type);
+        }
+        else{
+            os<< "\t"<< name<< " already exisits in the current ScopeTable\n";
+            //cout<< "\t'"<< name<< "' already exists in the current ScopeTable\n";
+            return false;
+        }
+    }
+
+    bool remove(string name){
+        return top->remove(name);
+    }
+
+    SymbolInfo* lookUp(string name){
+        ScopeTable* curr = top;
+        while(curr && !curr->lookUp(name,false)){
+            curr = curr->getParent();
+            if(!curr) break;
+        }
+        return (curr)? curr->lookUp(name) : top->lookUp(name);
+    }
+
+    void print(ostream& os, bool all=true){
+        ScopeTable* curr = top;
+        curr->print(os);
+        while(curr->getParent() && all){
+            curr = curr->getParent();
+            curr->print(os);
+        }
+        return;
+    }
+
+    friend ostream& operator<<(ostream& os, const SymbolTable dt){
+        ScopeTable* curr = dt.top;
+        while(curr){
+            curr->print(os);
+            cout<< endl;
+            curr = curr->getParent();
+        }
+        return os;
+    }
+
+    void errorCntMsg(char cmd){
+        cout<< "\tNumber of parameters mismatch for the command "<< cmd<< "\n";
+        return;
+    }
+
+    ~SymbolTable(){
+        ScopeTable* curr = top;
+        while(curr){
+            top = curr->getParent();
+            delete curr;
+            curr = top;
+        }
+    }
+};
+
